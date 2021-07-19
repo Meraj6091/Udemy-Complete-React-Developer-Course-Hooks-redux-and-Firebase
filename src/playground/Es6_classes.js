2de6e4f96064cfadad06801baee75f110783fdb6
Meraj Vindira <meraj.2017026@iit.ac.lk>
2021-07-19 22:01:32 +0530

    final commit

--- src/playground/Es6_classes.js
+++ src/playground/Es6_classes.js
@@ -1,15 +1,32 @@
 class Person {
-	constructor(name=0, age=0) {
+	constructor(name = 0, age = 0) {
 		this.name = name;
 		this.age = age;
 	}
-	methode() {
+	description() {
 		return `${this.name} is ${this.age} years Old`;
 	}
 }
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription() {
+		let description = super.description();
+		if (this.hasMajor()) {
+			description = `${this.name} is ${this.age} and Their major is ${this.major}`;
+		}
+		return description;
+	}
+}
+const C = new Student("Meraj", 21, "Software Enginner");
 const A = new Person("Meraj", 23);
 const B = new Person();
-console.log(A.methode());
+console.log(C.getDescription());
 function Es6_classes() {
 	return <div></div>;
 }
